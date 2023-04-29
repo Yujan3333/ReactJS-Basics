feceba9419ab1e2c0c2257cd78a20a8a467931ff
Yujan3333 <yujanbasnet18@gmail.com>
2023-04-29 13:10:24 +0545

    1st React Container

---
+++ index.js
@@ -0,0 +1,38 @@
+// ReactDOM is a global variable
+
+/*
+// Display a h1 in dom
+ReactDOM.render(<h1>Hello I am Yujan</h1>, document.getElementById("root"))
+*/
+
+
+/* 
+// Display a <ul> with 2 <li>
+ReactDOM.render(
+    <ul>
+        <li>Naruto</li>
+        <li>One Piece</li>
+        <li>Bleach</li>
+    </ul>
+    , document.getElementById("root")
+)
+*/
+
+
+/* 
+function MyComponent(){
+    return(
+        <h1>
+            I am Learning React
+        </h1>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <MyComponent />
+    </div>,
+    document.getElementById("root")
+)
+ */
+
